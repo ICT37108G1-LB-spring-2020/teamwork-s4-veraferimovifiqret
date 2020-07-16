82bec72ab3b4c377596c9fa5683a2ddbdfad82d3
SokraTT <eujirauli@gmail.com>
2020-07-16 06:50:12 +0400

    new modifed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('exam app is running!');
+    expect(page.getTitleText()).toEqual('shoe-store app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreComponent } from './store/store.component';
+import { AdminComponent } from './admin/admin.component';
+import { ShoesService } from './services/shoes.service';
+import { ShoeComponent } from './shoe/shoe.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { AddShoeComponent } from './add-shoe/add-shoe.component';
+import { EditShoeComponent } from './edit-shoe/edit-shoe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    StoreComponent,
+    AdminComponent,
+    ShoeComponent,
+    SpinnerComponent,
+    AddShoeComponent,
+    EditShoeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ShoesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
